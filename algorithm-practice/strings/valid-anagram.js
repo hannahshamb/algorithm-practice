c73694e344debc97e55025711c0caa4180ec9473
Hannah Engelhardt <hannahnengel@gmail.com>
2023-01-24 15:15:27 -0800

    Completed valid-anagram

---
+++ algorithm-practice/strings/valid-anagram.js
@@ -0,0 +1,24 @@
+/*
+Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+
+An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase,
+typically using all the original letters exactly once.
+*/
+var isAnagram = function (s, t) {
+  let anagram = true;
+  if (s.length !== t.length) {
+    return false;
+  }
+  let sCopy = s.split('').sort();
+  let tCopy = t.split('').sort();
+
+  for (let i = 0; i < sCopy.length; i++) {
+    if (sCopy[i] !== tCopy[i]) {
+      return false;
+    }
+  }
+
+  return anagram;
+}
+console.log('isAnagram("anagram", "nagaram")', isAnagram('anagram', 'nagaram'));
+console.log('isAnagram("rat", "car")', isAnagram('rat', 'car'));
