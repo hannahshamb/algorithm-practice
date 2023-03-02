28b3ae49ccad2d46417e7cae57fcff27adb7d8d6
Hannah Engelhardt <hannahnengel@gmail.com>
2023-03-02 12:11:29 -0800

    tweaked best time to buy and sell stock

--- algorithm-practice/arrays/best-time-buy-sell-stock.js
+++ algorithm-practice/arrays/best-time-buy-sell-stock.js
@@ -17,10 +17,10 @@ var maxSingleProfit = function (prices) {
   let profit = 0;
   for(let i = 0; i < prices.length - 1; i++) {
     for (let j = i + 1; j < prices.length; j++) {
-      const currentProfit = prices[j] - prices[i];
-      if (currentProfit > profit) {
-        profit = currentProfit;
-      }
+        const currentProfit = prices[j] - prices[i];
+        if (currentProfit > profit) {
+          profit = currentProfit;
+        }
     }
   }
   return profit;
