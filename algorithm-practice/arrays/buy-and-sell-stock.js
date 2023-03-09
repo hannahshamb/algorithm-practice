c6ccbcb90e810327e1e77afe72021e84b9034ed0
Hannah Engelhardt <hannahnengel@gmail.com>
2023-03-09 00:10:12 -0800

    Updated buy-and-sell-stock

--- algorithm-practice/arrays/buy-and-sell-stock.js
+++ algorithm-practice/arrays/buy-and-sell-stock.js
@@ -1,29 +1,44 @@
 /*
-You are given an array prices where prices[i] is the price of a given stock on the ith day.
-
-You want to maximize your profit by choosing a single day to buy one stock and
-choosing a different day in the future to sell that stock.
-
-Return the maximum profit you can achieve from this transaction.
-If you cannot achieve any profit, return 0.
-
+You are given an integer array prices where prices[i] is the price of
+a given stock on the ith day.
+On each day, you may decide to buy and/or sell the stock.
+You can only hold at most one share of the stock at any time.
+However, you can buy it then immediately sell it on the same day.
+Find and return the maximum profit you can achieve.
 */
 
-var maxProfit = function (prices) {
-  let buyingPrice = prices[0];
-  let sellingPrice;
+// LEVEL 1 - ONE BUY + SELL
+/* Input: prices = [7, 1, 5, 3, 6, 4]
+Output: 5
+*/
+var maxSingleProfit = function (prices) {
   let profit = 0;
-
-  for (let i = 1; i < prices.length; i++) {
-    sellingPrice = prices[i];
-    if (buyingPrice > sellingPrice) {
-      buyingPrice = sellingPrice;
-    } else {
-      profit = Math.max(sellingPrice - buyingPrice, profit);
+  for (let i = 0; i < prices.length - 1; i++) {
+    for (let j = i + 1; j < prices.length; j++) {
+      const currentProfit = prices[j] - prices[i];
+      if (currentProfit > profit) {
+        profit = currentProfit;
+      }
     }
   }
   return profit;
 };
 
-console.log('maxProfit([7, 1, 5, 3, 6, 4])', maxProfit([7, 1, 5, 3, 6, 4]))
-console.log('maxProfit([7, 6, 4, 3, 1])', maxProfit([7, 6, 4, 3, 1]))
+console.log(maxSingleProfit([7, 1, 5, 3, 6, 4]))
+
+
+// LEVEL 2 - MULTIPLE TRANSACTIONS
+// must sell the stock before you buy again
+/* Input: prices = [7, 1, 5, 3, 6, 4]
+Output: 7
+*/
+var maxMultiTransactionProfit = function (prices) {
+  let profit = 0;
+  for (let i = 0; i < prices.length - 1; i++) {
+    if (prices[i] < prices[i + 1]) {
+      profit += prices[i + 1] - prices[i];
+    }
+  }
+  return profit;
+};
+console.log(maxMultiTransactionProfit([7, 1, 5, 3, 6, 4]))
