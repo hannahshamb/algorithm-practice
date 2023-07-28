0488705abb8995b33c74e709d320c14bb47f0f95
Hannah Engelhardt <hannahnengel@gmail.com>
2023-07-27 17:36:00 -0700

    Completed a different approach to rotate-img

--- algorithm-practice/arrays/rotate-img.js
+++ algorithm-practice/arrays/rotate-img.js
@@ -15,6 +15,7 @@ Input: matrix = [[2,29,20,26,16,28],[12,27,9,25,13,21],[32,33,32,2,28,14],[13,14
 Expected Output: [[4,33,13,32,12,2],[24,1,14,33,27,29],[1,20,32,32,9,20],[6,7,27,2,25,26],[32,21,22,28,13,16],[34,7,26,14,21,28]]
 */
 
+//VERSION 1
 var rotate = function (matrix) {
 
   let right = matrix.length - 1;
@@ -56,3 +57,40 @@ console.log('after rotate(matrix1)', matrix1);
 const matrix2 = [[5, 1, 9, 11], [2, 4, 8, 10], [13, 3, 6, 7], [15, 14, 12, 16]];
 rotate(matrix2);
 console.log('after rotate(matrix2)', matrix2);
+
+
+//VERSION 2
+function rotate2(matrix) {
+  const n = matrix.length;
+  for (let layer = 0; layer < n / 2; layer++) {
+    const first = layer;
+    const last = n - 1 - layer;
+    for (let i = first; i < last; i++) {
+      const offset = i - first;
+      const top = matrix[first][i];
+      // left -> top
+      matrix[first][i] = matrix[last - offset][first];
+      // bottom -> left
+      matrix[last - offset][first] = matrix[last][last - offset];
+      // right -> bottom
+      matrix[last][last - offset] = matrix[i][last];
+      // top -> right
+      matrix[i][last] = top;
+    }
+  }
+}
+
+// Test case
+const matrix = [
+  [2, 29, 20, 26, 16, 28],
+  [12, 27, 9, 25, 13, 21],
+  [32, 33, 32, 2, 28, 14],
+  [13, 14, 32, 27, 22, 26],
+  [33, 1, 20, 7, 21, 7],
+  [4, 24, 1, 6, 32, 34]
+];
+
+rotate2(matrix);
+
+// Output: [[4,33,13,32,12,2],[24,1,14,33,27,29],[1,20,32,32,9,20],[6,7,27,2,25,26],[32,21,22,28,13,16],[34,7,26,14,21,28]]
+console.log('matrix', matrix);
