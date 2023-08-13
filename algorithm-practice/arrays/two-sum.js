49a3399a58e5d8982ea3fabb5eedd107319b209a
Hannah Engelhardt <hannahnengel@gmail.com>
2023-08-12 19:44:01 -0700

    Updated two-sum to complete with hash mapping

--- algorithm-practice/arrays/two-sum.js
+++ algorithm-practice/arrays/two-sum.js
@@ -20,6 +20,30 @@ function twoSum(nums, target) {
   }
 }
 
-console.log('twoSum([2, 7, 11, 15], 9)', twoSum([2, 7, 11, 15], 9));
-console.log('twoSum([3, 2, 4], 6)', twoSum([3, 2, 4], 6));
-console.log('twoSum([3, 3], 6)', twoSum([3, 3], 6));
+function twoSum2(nums, target) {
+  const numIndicesMap = {}; // Create an empty object to store numbers and their indices
+
+  for (let i = 0; i < nums.length; i++) {
+    const complement = target - nums[i]; // Calculate the complement needed to reach the target
+
+    if (complement in numIndicesMap) {
+      // If the complement is found in the map, return the current index and the stored index
+      return [numIndicesMap[complement], i];
+    }
+
+    // Store the current number and its index in the map
+    numIndicesMap[nums[i]] = i;
+  }
+
+  // If no solution is found, return an empty array or some other indicator
+  return [];
+}
+
+const nums = [2, 7, 11, 15];
+const target = 9;
+const result = twoSum2(nums, target);
+console.log(result); // Output: [0, 1]
+
+console.log('twoSum([2, 7, 11, 15], 9)', twoSum2([2, 7, 11, 15], 9));
+console.log('twoSum([3, 2, 4], 6)', twoSum2([3, 2, 4], 6));
+console.log('twoSum([3, 3], 6)', twoSum2([3, 3], 6));
