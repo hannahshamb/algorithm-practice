d53bb8be8dc2d61b96afdd19ec689ab1ae20bc16
Hannah Engelhardt <hannahnengel@gmail.com>
2023-08-31 18:12:36 -0700

    Completed merge-array

---
+++ algorithm-practice/arrays/merge-array.js
@@ -0,0 +1,23 @@
+var merge = function (nums1, m, nums2, n) {
+  let i = m - 1; // Pointer for nums1
+  let j = n - 1; // Pointer for nums2
+  let k = m + n - 1; // Pointer for merged array
+
+  while (i >= 0 && j >= 0) {
+    if (nums1[i] > nums2[j]) {
+      nums1[k] = nums1[i];
+      i--;
+    } else {
+      nums1[k] = nums2[j];
+      j--;
+    }
+    k--;
+  }
+
+  // If there are remaining elements in nums2, copy them to nums1
+  while (j >= 0) {
+    nums1[k] = nums2[j];
+    j--;
+    k--;
+  }
+};
