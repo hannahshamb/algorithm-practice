13868e17c96d930f49f018fa5c3f8499ff3ca446
Hannah Engelhardt <hannahnengel@gmail.com>
2023-10-02 15:02:19 -0700

    Completed return6to7

---
+++ algorithm-practice/math/return6to7.js
@@ -0,0 +1,13 @@
+//What Javascript statement in place of "?" will make the result always be between 6 and 7 ? const x = 2; let y = 4;
+
+const x = 2;
+let y = 4;
+
+function update(arg) {
+  return Math.random() + y * arg;
+}
+
+y = 3;
+
+const result = update(x);
+console.log(result)
